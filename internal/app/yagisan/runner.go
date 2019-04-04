2e11964de99fe462c8fc13083903a15417fe7933
Sota Yokoe <sota@kreuz45.com>
2019-04-04 22:31:01 +0900

    feat: Observe file writes.

--- internal/app/yagisan/runner.go
+++ internal/app/yagisan/runner.go
@@ -1,8 +1,52 @@
 package yagisan
 
-import "golang.org/x/xerrors"
+import (
+	"log"
+	"regexp"
+	"time"
+
+	"github.com/radovskyb/watcher"
+	"golang.org/x/xerrors"
+)
 
 // Run starts observing files.
 func Run() error {
-	return xerrors.New("not implemented")
+	return watchFileWrite(func(path string) {
+		log.Printf("File change: %v\n", path)
+	})
+}
+
+type fileChangeHandler func(string)
+
+func watchFileWrite(handler fileChangeHandler) error {
+	w := watcher.New()
+	w.SetMaxEvents(1)
+	w.FilterOps(watcher.Write)
+
+	r := regexp.MustCompile("(.*).go")
+	w.AddFilterHook(watcher.RegexFilterHook(r, false))
+
+	go func() {
+		for {
+			select {
+			case event := <-w.Event:
+				handler(event.Path)
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	if err := w.AddRecursive("."); err != nil {
+		return xerrors.Errorf("failed to add . recursively: %w", err)
+	}
+
+	log.Println("start watching files...")
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		return err
+	}
+
+	return nil
 }
